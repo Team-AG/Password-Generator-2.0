7053023cb76b2a0c190ef89f6ef667f21fcc429c
ANDREW <66812117+Team-AG@users.noreply.github.com>
2020-09-15 23:56:53 -0400

    Add files via upload

---
+++ bc-2020-h3/assets/javascript/main.js
@@ -0,0 +1,202 @@
+// Declare allCharacters object and assign nested arrays
+const allCharacters = [
+  [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ],
+  [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ],
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~"]
+];
+
+// Generate random password based on user specifications and print to screen
+function createPassword(options) {
+  // Use destructuring to get the checkboxes' value
+  const {
+    confirmLower,
+    confirmUpper,
+    confirmNumeric,
+    confirmSpecial
+  } = options;
+  // Use concatenation to generate an array of possible characters for the password based on which checkboxes the user selected
+  let passwordArray = [];
+
+  if (confirmLower) {
+    passwordArray = passwordArray.concat(allCharacters[0]);
+  }
+
+  if (confirmUpper) {
+    passwordArray = passwordArray.concat(allCharacters[1]);
+  }
+
+  if (confirmNumeric) {
+    passwordArray = passwordArray.concat(allCharacters[2]);
+  }
+
+  if (confirmSpecial) {
+    passwordArray = passwordArray.concat(allCharacters[3]);
+  }
+
+  // Use a loop to randomly select a character from the passwordArray and add it to the finalPassword until finalPassword reaches the length specified by the user
+  const passwordLength = document.getElementById("passwordLength");
+
+  let finalPassword = "";
+
+  for (var i = 1; i <= passwordLength.value; i++) {
+    const newCharacter =
+      passwordArray[Math.floor(Math.random() * passwordArray.length)];
+    finalPassword = finalPassword.concat(newCharacter);
+  }
+
+  // Print finalPassword to screen
+  const passwordDisplay = document.getElementById("displayBox");
+
+  passwordDisplay.innerText = finalPassword;
+
+  // Turn "Copy to Clipboard" button purple and create hover effect
+  const purpleButton = document.getElementById("copyToClipboard");
+  purpleButton.setAttribute("style", "background-color: var(--mainColor);");
+  purpleButton.onmouseover = function() {
+    purpleButton.setAttribute("style", "background-color: var(--darkColor);");
+  };
+  purpleButton.onmouseout = function() {
+    purpleButton.setAttribute("style", "background-color: var(--mainColor);");
+  };
+}
+
+// Check whether user checked at least one checkbox; If not, alert and prevent further action. If they did it correctly, proceed with generating a password.
+function checkboxError() {
+  const confirmLower = document.getElementById("confirmLower").checked;
+
+  const confirmUpper = document.getElementById("confirmUpper").checked;
+
+  const confirmNumeric = document.getElementById("confirmNumeric").checked;
+
+  const confirmSpecial = document.getElementById("confirmSpecial").checked;
+
+  if (
+    confirmLower === false &&
+    confirmUpper === false &&
+    confirmNumeric === false &&
+    confirmSpecial === false
+  ) {
+    alert("Error: Must select at least one type of character.");
+  } else {
+    createPassword({
+      confirmLower,
+      confirmNumeric,
+      confirmSpecial,
+      confirmUpper
+    });
+  }
+}
+
+// When user clicks Generate Password button, check whether user entered a number between 8 and 128 for password length; If not, error alert and prevent further action. If they did it correctly, proceed to checkbox error check.
+function lengthError() {
+  const passwordLength = document.getElementById("passwordLength");
+  if (
+    passwordLength.value < 8 ||
+    passwordLength.value > 128 ||
+    isNaN(passwordLength.value)
+  ) {
+    alert("Error: Password must have between 8 and 128 characters.");
+  } else {
+    checkboxError();
+  }
+}
+
+// Add event for user clicking "Generate Password" button
+document
+  .getElementById("generatePassword")
+  .addEventListener("click", lengthError);
+// value(L,U,Num,S) are my arrays
+// Generator functions
+function RandomL() {
+  return valueL[Math.floor(Math.random() * valueL.length)];
+}
+function RandomU() {
+  return valueU[Math.floor(Math.random() * valueU.length)];
+}
+function RandomNum() {
+  return valueNum[Math.floor(Math.random() * valueNum.length)];
+}
+function RandomS() {
+  return valueS[Math.floor(Math.random() * valueS.length)];
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+// Takes password from #displayBox div, converts it into a temporary text input element, selects it, copies it to clipboard and then deletes the input element
+function copyDisplayBox() {
+  const copyText = document.querySelector("#displayBox").innerText;
+  if (copyText === "Generate a password!") {
+    return;
+  } else {
+    const dummyInput = document.createElement("input");
+    dummyInput.type = "text";
+    dummyInput.value = copyText;
+    document.body.appendChild(dummyInput);
+    dummyInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummyInput);
+  }
+}
+
+// Add event for user clicking "Copy to Clipboard" button
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", copyDisplayBox);
