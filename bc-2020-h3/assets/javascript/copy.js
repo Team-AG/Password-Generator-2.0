5bd75d6d3490d834e33c5a7684bf6e54360f9b06
ANDREW <66812117+Team-AG@users.noreply.github.com>
2020-09-26 10:11:40 -0400

    Add files via upload

---
+++ bc-2020-h3/assets/javascript/copy.js
@@ -0,0 +1,20 @@
+// Takes password from #displayBox div, converts it into a temporary text input element, selects it, copies it to clipboard and then deletes the input element
+function copyDisplayBox() {
+    const copyText = document.querySelector("#displayBox").innerText;
+    if (copyText === "Generate a password!") {
+        return;
+    } else {
+        const dummyInput = document.createElement("input");
+        dummyInput.type = "text";
+        dummyInput.value = copyText;
+        document.body.appendChild(dummyInput);
+        dummyInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummyInput);
+    }
+}
+
+// Add event for user clicking "Copy to Clipboard" button
+document
+    .getElementById("copyToClipboard")
+    .addEventListener("click", copyDisplayBox);
